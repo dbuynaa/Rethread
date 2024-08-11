f41dfc135726d1f1570e98e22c8383d3b69d2dc2
Ben <62173996+dbuynaa@users.noreply.github.com>
2024-08-11 19:12:03 +0800

    sd

--- next.config.js
+++ next.config.js
@@ -2,15 +2,12 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-await import("./src/env.js");
+await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  compiler: {
-    styledComponents: true,
-  },
   //   experimental: {
   //     externalMiddlewareRewritesResolve: true,
   //   },
