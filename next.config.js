f3dde4e81b7234387e1b7a89dc2f62e188a1eb21
Ben <62173996+dbuynaa@users.noreply.github.com>
2024-08-15 18:35:12 +0800

    d

--- next.config.js
+++ next.config.js
@@ -2,17 +2,15 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-const execSync = require('child_process').execSync;
-const latestGitCommit = 'git rev-parse HEAD';
 await import('./src/env.js');
 
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
   swcMinify: true,
-  generateBuildId: async () => {
-    return execSync(latestGitCommit).toString().trim();
-  },
+  //   experimental: {
+  //     externalMiddlewareRewritesResolve: true,
+  //   },
 };
 
 export default config;
